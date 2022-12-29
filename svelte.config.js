f54e891d2666f281e13b8a5464ae382738d1d6a9
Shub <shub1493biswas@gmail.com>
2022-12-30 01:50:42 +0530

    feat: init repro

---
+++ svelte.config.js
@@ -0,0 +1,27 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter(),
+		// csp: {
+		// 	mode: 'auto',
+		// 	directives: {
+		// 		'default-src': ['self'],
+		// 		'img-src': ['self', 'data:'],
+		// 		'script-src': ['self', 'localhost', 'unsafe-inline'],
+		// 		'connect-src': ['self'],
+		// 		'style-src': ['self', 'unsafe-inline', 'fonts.googleapis.com'],
+		// 		'font-src': ['self', 'fonts.gstatic.com'],
+		// 		'base-uri': ['self'],
+		// 	}
+		// },
+	}
+};
+
+export default config;
